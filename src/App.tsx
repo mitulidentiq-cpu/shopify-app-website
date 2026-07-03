c409774b805fca9a1684a5576b0041305bf11442
mitulzalavadiya10-art <mitulzalavadiya10@gmail.com>
2026-07-03 12:23:27 +0530

    Initial commit: React + TS + Tailwind v4 project with TWBlocks custom Header and logo

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Header1 } from "@/components/ui/header"
+
+function App() {
+  return (
+    <div className="relative min-h-screen bg-background text-foreground">
+      {/* Header component integration */}
+      <Header1 />
+      
+      <main className="pt-24">
+        {/* Sections will be added here */}
+      </main>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ui/header.tsx
@@ -0,0 +1,177 @@
+import { Button } from "@/components/ui/button";
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+import { Menu, MoveRight, X } from "lucide-react";
+import { useState } from "react";
+
+import logo1 from "@/app logo/logo1.png"
+
+function Header1() {
+    const navigationItems = [
+        {
+            title: "Home",
+            href: "/",
+            description: "",
+        },
+        {
+            title: "Product",
+            description: "Managing a small business today is already tough.",
+            items: [
+                {
+                    title: "Reports",
+                    href: "/reports",
+                },
+                {
+                    title: "Statistics",
+                    href: "/statistics",
+                },
+                {
+                    title: "Dashboards",
+                    href: "/dashboards",
+                },
+                {
+                    title: "Recordings",
+                    href: "/recordings",
+                },
+            ],
+        },
+        {
+            title: "Company",
+            description: "Managing a small business today is already tough.",
+            items: [
+                {
+                    title: "About us",
+                    href: "/about",
+                },
+                {
+                    title: "Fundraising",
+                    href: "/fundraising",
+                },
+                {
+                    title: "Investors",
+                    href: "/investors",
+                },
+                {
+                    title: "Contact us",
+                    href: "/contact",
+                },
+            ],
+        },
+    ];
+
+    const [isOpen, setOpen] = useState(false);
+    return (
+        <header className="w-full z-40 fixed top-0 left-0 bg-background border-b border-border">
+            <div className="container relative mx-auto min-h-20 flex gap-4 flex-row lg:grid lg:grid-cols-3 items-center px-4">
+                <div className="justify-start items-center gap-4 lg:flex hidden flex-row">
+                    <NavigationMenu className="flex justify-start items-start">
+                        <NavigationMenuList className="flex justify-start gap-4 flex-row">
+                            {navigationItems.map((item) => (
+                                <NavigationMenuItem key={item.title}>
+                                    {item.href ? (
+                                        <>
+                                            <NavigationMenuLink href={item.href}>
+                                                <Button variant="ghost">{item.title}</Button>
+                                            </NavigationMenuLink>
+                                        </>
+                                    ) : (
+                                        <>
+                                            <NavigationMenuTrigger className="font-medium text-sm">
+                                                {item.title}
+                                            </NavigationMenuTrigger>
+                                            <NavigationMenuContent className="!w-[450px] p-4 bg-popover text-popover-foreground rounded-md border shadow-md">
+                                                <div className="flex flex-col lg:grid grid-cols-2 gap-4">
+                                                    <div className="flex flex-col h-full justify-between">
+                                                        <div className="flex flex-col">
+                                                            <p className="text-base font-semibold">{item.title}</p>
+                                                            <p className="text-muted-foreground text-sm">
+                                                                {item.description}
+                                                            </p>
+                                                        </div>
+                                                        <Button size="sm" className="mt-10">
+                                                            Book a call today
+                                                        </Button>
+                                                    </div>
+                                                    <div className="flex flex-col text-sm h-full justify-end">
+                                                        {item.items?.map((subItem) => (
+                                                            <NavigationMenuLink
+                                                                href={subItem.href}
+                                                                key={subItem.title}
+                                                                className="flex flex-row justify-between items-center hover:bg-muted py-2 px-4 rounded"
+                                                            >
+                                                                <span>{subItem.title}</span>
+                                                                <MoveRight className="w-4 h-4 text-muted-foreground" />
+                                                            </NavigationMenuLink>
+                                                        ))}
+                                                    </div>
+                                                </div>
+                                            </NavigationMenuContent>
+                                        </>
+                                    )}
+                                </NavigationMenuItem>
+                            ))}
+                        </NavigationMenuList>
+                    </NavigationMenu>
+                </div>
+                <div className="flex lg:justify-center">
+                    <img src={logo1} alt="App Logo" className="h-10 w-auto object-contain" style={{ filter: 'invert(1)' }} />
+                </div>
+                <div className="flex justify-end w-full gap-4 items-center">
+                    <Button variant="ghost" className="hidden md:inline-flex">
+                        Book a demo
+                    </Button>
+                    <div className="border-r h-6 hidden md:block border-border"></div>
+                    <Button variant="outline">Sign in</Button>
+                    <Button>Get started</Button>
+                </div>
+                <div className="flex w-12 shrink lg:hidden items-end justify-end">
+                    <Button variant="ghost" onClick={() => setOpen(!isOpen)} size="icon">
+                        {isOpen ? <X className="w-5 h-5" /> : <Menu className="w-5 h-5" />}
+                    </Button>
+                    {isOpen && (
+                        <div className="absolute top-20 border-t flex flex-col w-full right-0 bg-background shadow-lg py-4 px-4 gap-8 z-50">
+                            {navigationItems.map((item) => (
+                                <div key={item.title}>
+                                    <div className="flex flex-col gap-2">
+                                        {item.href ? (
+                                            <a
+                                                href={item.href}
+                                                className="flex justify-between items-center py-1"
+                                            >
+                                                <span className="text-lg font-medium">{item.title}</span>
+                                                <MoveRight className="w-4 h-4 stroke-1 text-muted-foreground" />
+                                            </a>
+                                        ) : (
+                                            <p className="text-lg font-medium">{item.title}</p>
+                                        )}
+                                        {item.items &&
+                                            item.items.map((subItem) => (
+                                                <a
+                                                    key={subItem.title}
+                                                    href={subItem.href}
+                                                    className="flex justify-between items-center pl-4 py-1 text-muted-foreground hover:text-foreground"
+                                                >
+                                                    <span>
+                                                        {subItem.title}
+                                                    </span>
+                                                    <MoveRight className="w-4 h-4 stroke-1" />
+                                                </a>
+                                            ))}
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export { Header1 };
